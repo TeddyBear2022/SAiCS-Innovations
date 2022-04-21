733227dc305dec9c32037b1ddb2b9ef8ff15550f
CB-Fiji <101645995+CB-Fiji@users.noreply.github.com>
2022-04-21 20:36:14 +0200

    FAQ page modifications

--- SAiCS/src/app/Client/faq/account-faq/account-faq.page.ts
+++ SAiCS/src/app/Client/faq/account-faq/account-faq.page.ts
@@ -11,5 +11,21 @@ export class AccountFaqPage implements OnInit {
 
   ngOnInit() {
   }
+  
+  display: boolean = false;
+  display2: boolean = false;
+  display3: boolean = false;
+
+  txtClick1(){
+    this.display = !this.display
+  }
+
+  txtClick2(){
+    this.display2 = !this.display2
+  }
+
+  txtClick3(){
+    this.display3 = !this.display3
+  }
 
 }
