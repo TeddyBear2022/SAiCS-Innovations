d139b35fc18bfc91bbc2e0e9d1ef33427db391d0
Amanda Sinamane <77048645+sinamane@users.noreply.github.com>
2022-09-22 13:48:30 +0200

    Saics App

---
+++ SaicsInnovations-MobileApp/src/app/Models/Ambassador.ts
@@ -0,0 +1,12 @@
+//Ambassador class
+export class Ambassador{
+    CourseID:number
+    BankaccountID:number
+    UserID: number
+    AmbassadorType: number
+    IDNumber:number
+    IDPhoto:number
+    ProofOfAddressPhoto:number
+    ProfilePic:number
+    ReferralCode:number
+}
---
+++ SaicsInnovations-MobileApp/src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    loadChildren: () => import('./User/login/login.module').then( m => m.LoginPageModule)
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'reset-password',
+    loadChildren: () => import('./User/reset-password/reset-password.module').then( m => m.ResetPasswordPageModule )
+  },
+  {
+    path: 'register',
+    loadChildren: () => import('./User/register/register.module').then( m => m.RegisterPageModule)
+  },
+  {
+    path: 'next',
+    loadChildren: () => import('./User/next/next.module').then( m => m.NextPageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  }
+
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
